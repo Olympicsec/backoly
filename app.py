816c0bbe264ff93aee67443444d4917dd08f3851
Olympicsec <Olympicsecuador@gmail.com>
2024-06-05 12:41:02 -0500

    cositas

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 import psycopg2
+from flask import request
 
 app = Flask(__name__)
 
 # Configuración de la conexión
 host = "35.223.204.64"
-database = "olympicsec"
+database = "postgres"
 user = "postgres"
 password = "admin"
 
@@ -32,11 +33,43 @@ def contact():
 
 @app.route('/donacion')
 def donation():
-    return render_template('donation1.html')
+    # Establecer la conexión
+    conn = psycopg2.connect(host=host, database=database, user=user, password=password)
+    # Crear un cursor
+    cur = conn.cursor()
+    
+    # Ejecutar la consulta SQL
+    cur.execute("SELECT * FROM Campanas")
+    
+    # Obtener los resultados
+    campanas = cur.fetchall()
+    
+    # Cerrar el cursor y la conexión
+    cur.close()
+    conn.close()
+    
+    return render_template('donation1.html', campanas=campanas)
 
-@app.route('/donacion_singular')
+@app.route('/donacion_singular', methods=['GET'])
 def donation_singular():
-    return render_template('donation-single.html')
+    campaign_id = request.args.get('id')  # Obtener el ID de la campaña desde los parámetros de la URL
 
+    # Establecer la conexión
+    conn = psycopg2.connect(host=host, database=database, user=user, password=password)
+
+    # Crear un cursor
+    cur = conn.cursor()
+
+    # Ejecutar la consulta SQL para obtener la campaña por su ID
+    cur.execute("SELECT * FROM Campanas WHERE ID_Campana = %s", (campaign_id,))
+
+    # Obtener el resultado
+    campana = cur.fetchone()
+
+    # Cerrar el cursor y la conexión
+    cur.close()
+    conn.close()
+
+    return render_template('donation-single.html', campana=campana)
 if __name__ == '__main__':
     app.run(debug=True)
